9e9936e9189852a917f0fcecc4173d4b2b3a08bf
El Hadi Moussi <elhadi.moussi@gmail.com>
2025-03-04 06:30:18 +0100

    Cleaning

--- tests/test_formview/test_applypipeview.py
+++ tests/test_formview/test_applypipeview.py
@@ -1,45 +1,41 @@
-from examples.formview.app import FormViewApp, Model
-from examples.formview.example import ExampleMainView
-from examples.formview.pipeapplyform import PipeApplyFormView
+from examples.formview.app import FormViewApp
 from examples.formview.pipeform import EdgeType
 
 import pytest
 
-from typing import Tuple
-
 
 @pytest.fixture()
-def app_apply_pipe(app: FormViewApp) -> Tuple[PipeApplyFormView, ExampleMainView, Model]:
+def app_apply_pipe(app: FormViewApp) -> FormViewApp:
     app.main_view.toolbar.navigation.pipe.is_checked = False
     app.main_view.toolbar.navigation.apply_pipe.is_checked = True
     app.main_view.toolbar.navigation.toggled()
     assert app.main_view.pipe_view.is_visible is False
     assert app.main_view.apply_pipe_view.is_visible is True
-    return app.main_view.apply_pipe_view, app.main_view, app.model
+    return app
 
 
-def test_unmodify_db_before_apply(
-    app_apply_pipe: Tuple[PipeApplyFormView, ExampleMainView, Model],
-) -> None:
-    view, _, model = app_apply_pipe
+def test_unmodify_db_before_apply(app_apply_pipe: FormViewApp) -> None:
+    view, model = app_apply_pipe.main_view.apply_pipe_view, app_apply_pipe.model
     assert model.apply_pipe_query.get_main_radius() == 80
     view.main.radius.field_view.value = 200
     view.main.radius.field_view.editing_finished()
     assert model.apply_pipe_query.get_main_radius() == 80
 
 
-def test_apply(app_apply_pipe: Tuple[PipeApplyFormView, ExampleMainView, Model]) -> None:
-    view, _, model = app_apply_pipe
+def test_apply(app_apply_pipe: FormViewApp) -> None:
+    view, model = app_apply_pipe.main_view.apply_pipe_view, app_apply_pipe.model
     assert model.apply_pipe_query.get_main_radius() == 80
     view.main.radius.field_view.value = 75
     view.apply_clicked()
     assert model.apply_pipe_query.get_main_radius() == 75
 
 
-def test_open_file_without_apply(
-    app_apply_pipe: Tuple[PipeApplyFormView, ExampleMainView, Model],
-) -> None:
-    view, main_view, model = app_apply_pipe
+def test_open_file_without_apply(app_apply_pipe: FormViewApp) -> None:
+    view, main_view, model = (
+        app_apply_pipe.main_view.apply_pipe_view,
+        app_apply_pipe.main_view,
+        app_apply_pipe.model,
+    )
     assert view.export.is_visible is True
     assert model.apply_pipe_query.get_export_path() is None
     assert view.export.field_view.text == ""
@@ -49,10 +45,8 @@ def test_open_file_without_apply(
     assert model.apply_pipe_query.get_export_path() is None
 
 
-def test_update_visibility_without_apply(
-    app_apply_pipe: Tuple[PipeApplyFormView, ExampleMainView, Model],
-) -> None:
-    view, _, model = app_apply_pipe
+def test_update_visibility_without_apply(app_apply_pipe: FormViewApp) -> None:
+    view, model = app_apply_pipe.main_view.apply_pipe_view, app_apply_pipe.model
     assert model.apply_pipe_query.get_name() != ""
     assert view.name.field_view.text != ""
     assert model.apply_pipe_query.get_edge_type() == EdgeType.Normal
@@ -64,10 +58,8 @@ def test_update_visibility_without_apply(
     assert model.apply_pipe_query.get_edge_type() == EdgeType.Normal
 
 
-def test_update_visibility_with_incorrect_data(
-    app_apply_pipe: Tuple[PipeApplyFormView, ExampleMainView, Model],
-) -> None:
-    view, _, model = app_apply_pipe
+def test_update_visibility_with_incorrect_data(app_apply_pipe: FormViewApp) -> None:
+    view, model = app_apply_pipe.main_view.apply_pipe_view, app_apply_pipe.model
     assert view.items is not None
     assert view.items.is_visible("chamfer") is False
     assert view.chamfer.is_visible is False
