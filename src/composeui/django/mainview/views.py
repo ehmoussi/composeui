5d145020177813606afc1e9f492065d04bec055b
El Hadi Moussi <elhadi.moussi@gmail.com>
2025-03-29 07:55:30 +0100

    Add navigation

--- src/composeui/django/mainview/views.py
+++ src/composeui/django/mainview/views.py
@@ -1,6 +1,9 @@
+from dataclasses import fields
+from typing import List
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from composeui.core.views.actionview import ActionView
 from composeui.mainview.views.mainview import MainView
 
 
@@ -10,7 +13,24 @@ def get_main_view() -> MainView:
     return getattr(settings, "CUI_MAIN_VIEW", MainView())
 
 
+def get_navigation_actions(main_view: MainView) -> List[str]:
+    actions = []
+    for nav_field in fields(main_view.toolbar.navigation):
+        if nav_field.type is ActionView:
+            nav_action = getattr(main_view.toolbar.navigation, nav_field.name)
+            assert isinstance(nav_action, ActionView)
+            actions.append(nav_action.text)
+    return actions
+
+
 def index(request: HttpRequest) -> HttpResponse:
     main_view = get_main_view()
-    print(main_view)
-    return render(request, "mainview/index.html", context={"title": main_view.title})
+    navigation_actions = get_navigation_actions(main_view)
+    return render(
+        request,
+        "mainview/index.html",
+        context={
+            "title": main_view.title,
+            "navigation_actions": navigation_actions,
+        },
+    )
--- src/examples/probamodelapp/probamodelapp/mainview.py
+++ src/examples/probamodelapp/probamodelapp/mainview.py
@@ -1,7 +1,25 @@
+from dataclasses import dataclass, field
+from composeui.core.views.actionview import ActionView
+from composeui.mainview.views.maintoolbar import MainToolBar
 from composeui.mainview.views.mainview import MainView
+from composeui.mainview.views.toolbar import CheckableToolBar
 
 
-class ProbaModelMainView(MainView):
+@dataclass(eq=False)
+class ProbaModelNavigation(CheckableToolBar):
+    definition: ActionView = field(init=False, default_factory=ActionView)
+
     def __post_init__(self) -> None:
         super().__post_init__()
-        self.title = "ProbaModelApp"
+        self.definition.text = "Definition"
+
+
+@dataclass(eq=False)
+class ProbaModelMainToolBar(MainToolBar):
+    navigation: ProbaModelNavigation = field(init=False, default_factory=ProbaModelNavigation)
+
+
+@dataclass(eq=False)
+class ProbaModelMainView(MainView):
+    title = "ProbaModelApp"
+    toolbar: ProbaModelMainToolBar = field(init=False, default_factory=ProbaModelMainToolBar)
